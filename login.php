512d27a0b834c140300ae8fab26bf7ccb8491c55
AlperenAysel <48491546+AlperenAysel@users.noreply.github.com>
2020-12-28 21:44:38 +0300

    Textboxları temizleyen kod eklendi

--- login.php
+++ login.php
@@ -44,14 +44,14 @@ if (isset($_POST['email'], $_POST['password'])) {
         <form action="" method="post">
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input name="email" type="email" class="form-control" id="exampleInputEmail1"
+                <input name="email" type="email" class="form-control" id="exampleInputEmail1" onfocus="this.value=''"
                     aria-describedby="emailHelp" placeholder="Enter your email address..">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                     else.</small>
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
-                <input name="password" type="password" class="form-control" id="exampleInputPassword1"
+                <input name="password" type="password" class="form-control" id="exampleInputPassword1" onfocus="this.value=''"
                     placeholder="Enter your password..">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
